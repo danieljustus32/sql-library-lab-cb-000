ae194507e14572170b0e9cc176dc91f7348edebe
Daniel Justus <daniel.justus32@gmail.com>
2017-12-02 00:02:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series JOIN characters ON characters.author_id = series.author_id GROUP BY(characters.species) ORDER BY MAX(characters.species) DESC LIMIT 1;"
+  "SELECT title FROM series JOIN characters ON characters.author_id = series.author_id GROUP BY(characters.species) MAX(characters.species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
